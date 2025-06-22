ed376957d23120f7961b3bc6210d1fc861ffa347
Stephen Hurd <deuce@synchro.net>
2025-06-22 15:30:45 -0400

    Specify the type...

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,8 @@
 
 pack () {
 	local name=$1
-	echo node_modules/.bin/fvtt package --in "${name}" --out packs pack "${name}"
-	node_modules/.bin/fvtt package --in "${name}" --out packs pack "${name}"
+	echo node_modules/.bin/fvtt package --type Module --in "${name}" --out packs pack "${name}"
+	node_modules/.bin/fvtt package --type Module --in "${name}" --out packs pack "${name}"
 }
 
 rm -rf packs
